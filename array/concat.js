9cc327b235a0be4278dca00e80f3c19337638771
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-29 15:14:06 +0530

    concatination of the array

---
+++ array/concat.js
@@ -0,0 +1,9 @@
+const array  = ["aman", "shivendra", "sahil"]; 
+
+const array2 = ["ayush", "dalal","mehul"]
+
+const array3 = array2.concat(array)
+console.log(array3);
+
+
+///concat use for join the array it self
