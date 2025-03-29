eea33810ccacd01354dfafd892a357bfbbcad4b0
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-29 17:53:31 +0530

    find in the array

---
+++ array/find.js
@@ -0,0 +1,6 @@
+const array =  [{id: 2, name: "aditya rawat"},{id:3 , name: "kailash"}]
+
+const findedElement = array.find(element => element.id ==2)
+console.log(findedElement);
+
+/// in the last i think i got the point we can find the each element so called pro gamer
