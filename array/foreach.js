8f794ff14db83102260a40096d9b85b090402ba9
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-29 15:05:58 +0530

    foreach

---
+++ array/foreach.js
@@ -0,0 +1,7 @@
+const array  = ["aman", "shivendra", "sahil"];
+
+array.forEach((index)=>{
+    console.log(index)
+})
+
+/// conosle the the all element each time index only is the method to grab the array element
