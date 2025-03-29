5652e6b749166cfda90aacb98c10ecd6daf13cf6
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-29 17:39:55 +0530

    filll method in the array

---
+++ array/fill.js
@@ -0,0 +1,7 @@
+/// declaration of array
+let array = [1,2,3,4,5,6,7,8,9,0]
+
+const filledarray = array.fill(9,2,5)
+console.log(filledarray)
+
+/// the fill method fill the all stactic value from start to end of given index
