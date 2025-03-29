c48e8dcd1837db8656d8e29cc43c956d38dc220d
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-29 14:36:46 +0530

    at.js updated

---
+++ array/at.js
@@ -0,0 +1,8 @@
+const array = ["aditya", "aman", "shivendra"]
+
+//// th
+const whatelementintthetwo = array.at(2)
+console.log(whatelementintthetwo)
+
+/// what i learn 
+/// at alove to see the position what are the value situated in the index 
