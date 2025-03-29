78f4b035afcc1dce3c5fe560c1f00082594973ed
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-29 14:28:17 +0530

    first commit

---
+++ Loop/forloop.js
@@ -0,0 +1,7 @@
+// for (var i = 0;i <=5 ; i++)  {
+//     console.log("Hellowordd", i)
+// }
+
+for(i= 0; i <=10 ; i++){
+    console.log("finalyy its working", i)
+}
---
+++ Loop/whileloop.js
@@ -0,0 +1,7 @@
+    const index = 10;
+const language = "Hindi"
+    while (language == "Hindi"){
+        console.log("while use check the value only")
+    }
+
+    //// in a loop we are creating the variable and comaparing with the value for ending the loop and finalyy we are substractin and adding for end the loop
