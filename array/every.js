9478a2afa79dbf4202fb52a813f4e2280a34bedb
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-29 17:33:09 +0530

    what i learned in every array

--- array/every.js
+++ array/every.js
@@ -9,3 +9,7 @@ let array2 = ["pear", "kiwi", "plum", "date"];
 const arrayResult2 = array2.every(arr => arr.length === 4);
 
 console.log(arrayResult2); // Output: true ✅
+
+
+/// what i learn we can check the legth and something whatever for the array elements
+// we can clearly see in the result of code line number 3 we are checking the length of word 
