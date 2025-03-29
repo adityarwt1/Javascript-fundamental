ac2570429d173fcc89243b66ab53b32fd3fb0c3e
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-29 16:10:26 +0530

    copy within is completed

---
+++ array/copywithin.js
@@ -0,0 +1,15 @@
+  const arr = [1,2,3,4,5,6,7]
+  console.log(arr.copyWithin(2,0, 3)); /// arr.copywithin(target,start,end)
+/// 1,2,1,2,3,6,7
+
+//what i learn in this fuction 
+
+/// copywithing work like target, start,end 
+
+/// we are copy the array item into the element from 0 to 2 index of array after in the the index three removing the for and 
+
+
+//output is 
+//   1, 2, 1, 2,
+//   3, 6, 7
+  
