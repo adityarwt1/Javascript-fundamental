744150578726122e23e736503f060fb999da83cf
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-30 08:21:09 +0530

    map in the array

--- array/map.js
+++ array/map.js
@@ -1,9 +1,9 @@
-const array = ["aditya", "aman", "shivendra"] 
+const array = ["aditya", "aman", "shivendra"]
 
 let ul = document.createElement("ul")
- for(let i =0; i< array.length ; i++){
-  const list =   document.createElement("li")
-  list.textContent = array[i]
-  ul.appendChild(list)
+for (let i = 0; i < array.length; i++) {
+    const list = document.createElement("li")// first we are creatting the list 
+    list.textContent = array[i] /// this is the main i didnot not about the array how to print as well
+    ul.appendChild(list)
 }
 document.body.appendChild(ul)
