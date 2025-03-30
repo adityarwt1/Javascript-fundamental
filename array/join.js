85919811391fa143ee99f2d85b322e6dfde582fb
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-30 08:00:50 +0530

    join.js

---
+++ array/join.js
@@ -0,0 +1,7 @@
+const array = ["apple","papaya"]
+
+const joinedarray = array.join("-")
+console.log(joinedarray);
+
+//the output when do that is apple-papaya 
+//join mainly join the each element when the segment we are passing the method to joinning the array
