f5a206a6dc553c861e426e39741dd678f356b204
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-30 08:55:21 +0530

    reverse in array

---
+++ array/reverse.js
@@ -0,0 +1,7 @@
+const array = ["aditya", "aman", "shivendra"]
+
+const reverseArray =  array.reverse()
+console.log(reverseArray);
+
+
+/// this is revesing the array output will be [ 'shivendra', 'aman', 'aditya' ]
