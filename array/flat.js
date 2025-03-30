166d3268a4fa7b5ac8d461c072525bac3433ba36
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-30 07:46:23 +0530

    flat in the array

---
+++ array/flat.js
@@ -0,0 +1,11 @@
+const unflated = [1,2,[3,4,5],6,[7,8,9]]
+
+const flattedarray= unflated.flat()
+
+console.log(flattedarray)
+
+
+/// when we are use fla method to make a clean arraay elements 
+/// when we use flat() empty it removes emty slot from the array and 
+/// when we use flat(1) making array flat level 1 only
+/// when we use flat(infinity) make fully flat all time
