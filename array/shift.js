c6989beb66d4ceaf5e0d710086304d956e3a179b
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-30 09:03:20 +0530

    set interval and timeout

---
+++ array/shift.js
@@ -0,0 +1,10 @@
+const array = ["aditya", "aman", "shivendra"];
+
+
+const shiftedArray = array.shift()
+console.log(array)
+
+
+//// remove the firsh element from the array
+
+console.log(shiftedArray);
---
+++ development/setinterval.js
@@ -0,0 +1,9 @@
+const interval = ()=>{
+    console.log("set interval")
+}
+setInterval(interval, 1000)
+
+const timeout = ()=>{
+    console.log("set interval")
+}
+setTimeout(timeout,1000)
