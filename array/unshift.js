f8f736d9194d1a660741d9ca134bf45be6ccfe03
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-30 09:28:14 +0530

    unshift modified

--- array/unshift.js
+++ array/unshift.js
@@ -5,3 +5,6 @@ console.log(unshiftedItem);
 
 //// The unshift(value) method adds a new element to the beginning of the array.
 ///The console logs 4, NOT [2, "aditya", "aman", "shivendra"], because unshift() returns the array length, NOT the updated array.
+
+// console.log(array);
+// // Output: [2, "aditya", "aman", "shivendra"] ✅
