07d699b590166861ff581540126e5a70deda8a59
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-03 15:41:56 +0530

    revisionning the at in the array

--- array/at.js
+++ array/at.js
@@ -3,6 +3,6 @@ const array = ["aditya", "aman", "shivendra"]
 //// th
 const whatelementintthetwo = array.at(2)
 console.log(whatelementintthetwo)
-
+/// output will wbe shivendra 
 /// what i learn 
 /// at alove to see the position what are the value situated in the index 
