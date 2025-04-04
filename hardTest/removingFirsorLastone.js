3a162796e8a2f68c935d45edb25344504551c4ca
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-04 10:32:06 +0530

    array related questions

---
+++ hardTest/removingFirsorLastone.js
@@ -0,0 +1,11 @@
+const myArray = [["John", 23], ["cat", 2]];
+
+/// for removing the last items to the string
+const removedFromMyArray = myArray.shift()
+myArray.pop()
+
+
+
+// for result checking in the console
+console.log(myArray.pop())
+console.log(myArray.shift());
---
+++ hardTest/subarray.js
@@ -0,0 +1,9 @@
+var myList = [
+    ["Canned Beans", 3],
+    ["Milk Galon", 1],
+    ["Cereal", 2],
+    ["Toilet Paper", 12],
+    ["Sack of Rice", 1]
+  ];
+
+  /// subarray mean array into the arrray it meand inside 
