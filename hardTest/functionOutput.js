f41522984bfc6eef1ad705a3ad78fd47198ff046
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-04 13:57:56 +0530

    function oputput understanding

---
+++ hardTest/functionOutput.js
@@ -0,0 +1,16 @@
+// Setup
+let sum = 0;
+function addThree(num) {
+  sum = num + 3;
+}
+// Only change code below this line
+function addFive(sum) {
+  sum += 5;
+}
+// Only change code above this line
+
+addThree(5);
+addFive(3);
+
+/// what i lerned from the test w
+// when can reasing the sum by in the funcrtion witout effecttin any kind of stuff
