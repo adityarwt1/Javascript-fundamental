f8c53185fade7c6a09d7578d0527c50538032194
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-04 14:04:58 +0530

    assining the return value

---
+++ hardTest/asignreturnedValue.js
@@ -0,0 +1,13 @@
+// Setup
+let processed  = 2;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+
+
+/// mistake i made i learn in the when i learning this returning the value functionality to asing into the processed
+/// i think i give any value in the num code line number 4, but we need to make equal value for asigning
