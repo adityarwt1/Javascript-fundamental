fa55d914ed354981a0d3f57a1d0f727021792cee
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-07 09:15:59 +0530

    switch case in javvascrript

--- hardTest/returnAgsucation.js
+++ hardTest/returnAgsucation.js
@@ -1,17 +1,12 @@
 function orderMyLogic(val) {
-    if (val < 10) {
-      return "Less than 10";
-    } else if (val < 5) {
+    if (val < 5) {
       return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
     } else {
       return "Greater than or equal to 10";
     }
   }
-  
-  orderMyLogic(7);
-  orderMyLogic(4);
-  orderMyLogic(6);
-  orderMyLogic(11);
 
 
   /// what i learn from this test 
