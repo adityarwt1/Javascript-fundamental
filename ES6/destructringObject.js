cebdf6ba59a33f3e1e66dd7e1cb03e782c522406
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-08 20:12:14 +0530

    destuctering with another method

--- ES6/destructringObject.js
+++ ES6/destructringObject.js
@@ -8,6 +8,10 @@ const HIGH_TEMPERATURES = {
   
   const {today} = HIGH_TEMPERATURES; //// we can access the object inside element by using curly bracet on the site 
   const {tomorrow} = HIGH_TEMPERATURES;
+
+  //also we can use this another syntax to access the inside as a variable
+
+  const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
   
   // Only change code above this line
 
