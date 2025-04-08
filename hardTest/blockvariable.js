12968a02400bcd0df8c53e2463fe2b02c1d2e0f0
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-08 19:28:45 +0530

    object freeze in the es6

--- hardTest/blockvariable.js
+++ hardTest/blockvariable.js
@@ -1,13 +1,15 @@
-const i = "function scope";
+function globalVariable(){
+    let i = "function variable";
 
+    if(true){
+        let i = "blocked scope";
+        console.log("this is inside if",i)
+        
+    }
+    return console.log(i)
 
-if(true){
-    let i = "blocked scope"
-    console.log("this is blocked scope forever", i)
 }
+globalVariable()
 
-console.log(i)
 
-
-/// console returnn the statement funtion scope becase if is a blocake elemene of javascript.
-//output will be the function scope
+///es6 ccan have block variable in the if condition 
