c8203dd8f2d8dc6e4925ea91731e92c1b112d8dc
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-08 19:56:52 +0530

    spread operator in the es6 jvascript

---
+++ ES6/spreadOperator.js
@@ -0,0 +1,12 @@
+const  calucation =(...n) =>{
+    const total =0;
+    for(let i = 0; i< n.length; i++){
+        total += n[i]
+    }
+    return total
+}
+console.log(calucation(2,43,4,5,6,6,7,7))
+
+//// ... thtrr dot called spread operator it is mainly use to get unlimmited parameter for the value
+
+/// n treat like a string who have length and index both
