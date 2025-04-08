f482cdc0437a6792c2d4692f553b462ad50065a7
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-08 08:32:16 +0530

    loop was completed almost

---
+++ hardTest/whileLoop.js
@@ -0,0 +1,13 @@
+// Setup
+const myArray = [];
+let i = 5;
+
+while(i>=0){
+  myArray.push(i)
+  i--
+
+}console.log(myArray);
+
+
+//// i make the mistake before i give the logic for the loppt was wrong i don't provide equal too sign in the logic bracett
+// Only change code below this line
