a0a9eda7e10972e9408d93534a4b4a9e2e332d95
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-08 08:34:42 +0530

    for loop

---
+++ hardTest/forLoop.js
@@ -0,0 +1,10 @@
+// Setup
+const myArray = [];
+for(let i = 1; i <= 9; i += 2){
+    myArray.push(i)
+}
+console.log(myArray)
+// Only change code below this line
+
+
+/// i made mistakee when i giving the logic for the loop i can;t use assigment operator for the end of the loop
