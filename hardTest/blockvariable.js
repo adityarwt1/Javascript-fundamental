41eaddde13e9f51a786de37a4c1e9293886e066c
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-08 12:26:50 +0530

    gettin knolwdge of blockd scope

---
+++ hardTest/blockvariable.js
@@ -0,0 +1,7 @@
+const i = "function scope";
+
+
+if(true){
+    let i = "blocked scope"
+    console.log("this is blocked scope forever")
+}
--- hardTest/nestedsubarrayLopp.js
+++ hardTest/nestedsubarrayLopp.js
@@ -9,8 +9,10 @@ function multiplyAll(arr) {
     // Only change code above this line
     return product;
   }
-  
-  // Modify values below to test your code
+  const happy = "happyu"
+  if(typeof happy === "string"){
+    console.log("happy is happy because")
+  }
   multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
-
+/// 
   /// inn there funtion of loops who are first getting thhe index from first lopp and second loop getting the item in whic array have at ttthe at we are gettting the product of the array which have
