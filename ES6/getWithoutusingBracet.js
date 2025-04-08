3222a3eeab45bd2ddf42d8a516fedab34a26b3e8
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-08 20:39:27 +0530

    getting the result without using key: value

---
+++ ES6/getWithoutusingBracet.js
@@ -0,0 +1,11 @@
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {
+      name, name,
+      age, age,
+      gender, gender
+    };
+    // Only change code above this line
+  };
+
+  /// we camn clearly seen we can easily access the name , name instead of using name: name
