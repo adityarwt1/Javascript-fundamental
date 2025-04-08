71dd32efe9444e9a9b5c669855dec46096ba68ea
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-08 20:20:35 +0530

    swappoing the value and destructering method in the es6

--- ES6/destructringObject.js
+++ ES6/destructringObject.js
@@ -16,3 +16,20 @@ const HIGH_TEMPERATURES = {
   // Only change code above this line
 
 
+
+  //// 
+  //
+  const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  // Only change code below this line
+    
+  const {today: {low: lowToday}} = LOCAL_FORECAST;
+  const {today: {high}} = LOCAL_FORECAST;/// this the difreent way to access the object elment in the difrent level
+  
+  // Only change code above this line
+
+
---
+++ ES6/swapping.js
@@ -0,0 +1,7 @@
+const a = 8;
+const b = 6;
+
+[a,b] = [b,a]
+
+
+///// swapping the value with array doesnot even matter to number and string declaration int es6
