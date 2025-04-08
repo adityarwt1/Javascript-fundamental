0bd50f5276b8de2d7db54346bc376aec86af2992
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-08 21:52:56 +0530

    test the avlaiblity into the string

---
+++ Regular Expressions/test.js
@@ -0,0 +1,6 @@
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
+
+
+/// there we are test it means we are chec=kjk the avalaiblity in the string or not
