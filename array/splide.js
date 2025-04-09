4f69c488f4ceb177f0efb727d7653ff958bd667f
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-09 11:42:45 +0530

    splice ho ya slice ho apane ko kya karana hai

---
+++ array/splide.js
@@ -0,0 +1,16 @@
+const array = ["aditya", "aman", "shivendra"]
+
+const slicedArray = array.slice(1,2)
+
+console.log(slicedArray);
+
+
+////
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// only change code below this line
+arr.splice(1, 4);
+// only change code above this line
+console.log(arr);
+
+/// splice mainly delete give value form the array or the range
+/// it means it get the only anrea like a bread slice
