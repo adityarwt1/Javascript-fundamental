d7a31b2365ce6afe5b68ff24b458a2a9c2b4b29d
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-09 09:09:50 +0530

    all world included or not

---
+++ Regular Expressions/allIncludedword.js
@@ -0,0 +1,7 @@
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
+
+
+
+///w question be like can you tell me all word are included or not 
