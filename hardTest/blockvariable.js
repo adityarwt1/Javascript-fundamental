26b8ef61ca47a15b97e22488c610835ff0fcb4fe
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-10 20:59:41 +0530

    input field error ( value)

--- hardTest/blockvariable.js
+++ hardTest/blockvariable.js
@@ -1,3 +1,4 @@
+
 function globalVariable(){
     let i = "function variable";
 
---
+++ realUse/alertInform/whatILearned.js
@@ -0,0 +1,6 @@
+//// my mistake whem i create this prpject
+// first i check the input fiel by textConten and innerHtml
+/// 
+
+//// solution and what i learned from this project 
+//// we can select the input field value by value
