06a79760c06aaa5ace66001b1bf1562152e37569
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-10 20:18:01 +0530

    kuchh to modified

--- Regular Expressions/test.js
+++ Regular Expressions/test.js
@@ -4,3 +4,8 @@ let result = myRegex.test(myString); // Change this line
 
 
 /// there we are test it means we are chec=kjk the avalaiblity in the string or not
+
+// let myString = "freeCodeCamp";
+// let fccRegex = /freeCodeCamp/i;
+// let result = fccRegex.test(myString);
+
