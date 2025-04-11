bac0fcd2b3d4a7d3af60585bc364f7a8e37e65a1
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-11 08:32:29 +0530

    dir for debugging

---
+++ console/dir.js
@@ -0,0 +1,13 @@
+const user = {
+    name: "Aditya",
+    age: 17,
+    skills: ["JavaScript", "Next.js", "MongoDB"]
+  };
+  
+  console.log(user);
+  
+  const el = document.getElementById("myDiv");
+  console.dir(el);
+
+
+  //// mainly i could understand this is useless methood to get print the output of dir
