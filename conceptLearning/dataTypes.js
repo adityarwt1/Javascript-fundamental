cf4d297f7cf3d078aaaa510c4965716939987624
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-11 09:18:18 +0530

    data tyep complited

---
+++ conceptLearning/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" // use all js code to modern javascrript
+
+/// alert(3+3) /// this will not thive the output because this is node not the console of browser
+
+/// make code readiblity
+
+/// data type 
+
+let name  = "aditya rawat";
+let age = 17;
+let isLoggedIn = true;
+let StandAlone = null; /// represted the empty string /// example we don't get wether value /// 
+let undifienedValue  = undefined;
+let sybolType = unique;
+let object  = {
+    name: "Aditya Rawat",
+    age: 17
+}
+
+console.log(typeof null ); // output will vve object because null is a object according to the javascript
