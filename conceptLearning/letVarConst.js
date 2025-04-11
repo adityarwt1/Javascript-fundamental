0e01c89e152945d43eb3d09bcc839a3dd3f953bf
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-11 08:46:36 +0530

    let const var

--- conceptLearning/letVarConst.js
+++ conceptLearning/letVarConst.js
@@ -8,3 +8,16 @@ var accountPassword = "aditya"
 console.assert(accountId)
 
 console.table([accountId,accountEmail,accountPassword])
+
+
+
+//// {} this called scop[e ]  in the javascript
+
+let functionScpope = "function Scope";
+let condition = true;
+if (2+2 === 4) {
+    functionScpope = "bloced scope"
+    console.log(functionScpope)
+}
+
+console.log(functionScpope)
