ee86d2e23624971a2d923466784556bdadd7f8aa
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-11 08:14:19 +0530

    assert in javascript

---
+++ conceptLearning/letVarConst.js
@@ -0,0 +1,10 @@
+//// va
+
+
+const accountId = "12345987"
+let accountEmail = "adityarawatnew2487@gmail.com"
+var accountPassword = "aditya"
+
+console.assert(accountId)
+
+console.table([accountId,accountEmail,accountPassword])
---
+++ console/asseration.js
@@ -0,0 +1,5 @@
+/// definition: console.assert() in JavaScript is a debugging tool used to write assertions — expressions that you expect to be true. If the expression evaluates to false, it logs an error message to the console.P
+
+console.assert(2+2 == 4,"Nothing should be happeng"); ///// nothin igive the output
+
+console.assert(25 + 2 == 28 , "Math is Broken" ) /// this give error massage or any thing you want when assert is failed
