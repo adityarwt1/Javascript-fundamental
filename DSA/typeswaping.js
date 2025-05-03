f7fbddd4a7a8c651ed79f9fa04e9d4b608d65e33
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-03 15:43:30 +0530

    base 64 added innto the developement foldder which can convert the image into the base 64 format which have to convert into the url then show the preview of imagge into the browser

---
+++ DSA/typeswaping.js
@@ -0,0 +1,14 @@
+/// type  swaping  
+
+
+let a = 10;
+let b  = 20 ;
+
+
+
+a= a + b /// i means we have a = 30 ( a inistial value is 10)
+b = a - b; /// this we already b value but we substract the value to get the a vlaue in the b variable 
+a = a- b ; // we are doing this beacuse as we know the a value i 30 in continue show we are substractinng the value of b afete declration of b and geting the last value of 10 in the b
+
+console.log(a)
+console.log(b)
---
+++ Dovelopement/filetobase64.js
@@ -0,0 +1,36 @@
+function fileToBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+
+        reader.onload = function () {
+            resolve(reader.result);
+        };
+
+        reader.onerror = function (error) {
+            reject(error);
+        };
+
+        reader.readAsDataURL(file);
+    });
+}
+
+
+const ownEffort = (file) => {
+    /// fucntion return promise so we use resolve and reject as well 
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+
+        //// thhis function return changes in the file  
+        reader.onload = function () {
+            resolve(reader.result)
+        }
+
+        /// this function also return the error when porforming the functon error int promise
+        reader.onerror = function (error) {
+            reject(this.error)
+        }
+
+        /// this step contai  the file fonmat converted into the base64 
+        reader.readAsDataURL(file)
+    })
+}
