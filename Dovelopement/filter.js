d789b11df989bf179b984c6dffb02f512d12ca93
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-04 21:39:35 +0530

    i learned how to fileter the search

---
+++ Dovelopement/filter.js
@@ -0,0 +1,40 @@
+export async function GET(request) {
+    try {
+        await connectDB();
+
+        const searchParams = req.nextUrl.searchParams;
+        const query = searchParams.get('query') || '';
+        const category = searchParams.get('category') || '';
+
+        let filter= {};
+
+        if (query) {
+            filter.$or = [
+                { title: { $regex: query, $options: 'i' } },
+                { description: { $regex: query, $options: 'i' } }
+            ];
+        }
+
+        if (category) {
+            filter.category = category;
+        }
+
+        const recipes = await Recipe.find(filter);
+
+        if (!recipes || recipes.length === 0) {
+            return NextResponse.json(
+                { success: false, error: "No recipes found" },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json({ recipes }, { status: 200 });
+
+    } catch (error) {
+        console.error("Error fetching recipes:", error);
+        return NextResponse.json(
+            { success: false, message: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+}
