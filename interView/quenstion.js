f53b7508382ad2939c45bcd80f40edd492bc53fe
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-08 16:15:34 +0530

    how tto use loading page into recipie iamge tab ]

--- interView/quenstion.js
+++ interView/quenstion.js
@@ -2,4 +2,5 @@ const que1 = "what iss the diffrence between var variable and const variable"
 const ans1 = 'i) we can use variable before instioaling when use var but not for let '
 const que2 = "what is diffrecne between undifiend and not defiend" //// undefined a value or not defined a error whch make code stuck throw the error
 const que3 = 'What happend when you try to access the variable value before intilizing?'
-const que4 = 'what is'
+const que4 = "custom layout in the next js"
+const que5 = "how to use loading page into next js"
