aee963e36bbf3b2d9eb2f055dc073f7f753842ef
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-08 22:26:31 +0530

    super of next js in one file

---
+++ nextjs/superPower.js
@@ -0,0 +1,3 @@
+const superpower1 = "useSWR in next js "
+const superpower2 = "nextUrl is the best way to get the information"
+const superpower3 = "loading page default provided to stop clutring of the code"
