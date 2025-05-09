56c89f0e8d213aa040703b380a9fdad3414eb130
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-09 21:21:46 +0530

    english communication improved

--- EnglishCommunication/script.js
+++ EnglishCommunication/script.js
@@ -1,2 +1,7 @@
 const situation1 = "when need to say age badhate hai "
 const answer1 = "let's go ahead"
+
+const situation2 = "jab bhi dikhe ki ab nahi ho raha communication "
+const answer2 = "start talkin about what you written recently in this code"
+
+const situation3 = ""
