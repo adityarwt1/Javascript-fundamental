d8eff01424c73628316b8d0cbf53a88a28a184f3
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-10 15:09:24 +0530

    bind concept is clear

---
+++ AdvanceTopic/bindContext.ts
@@ -0,0 +1,11 @@
+const person = {
+    name: "Aditya",
+    greeet() {
+        console.log(`my name is ${this.name}`)
+    }
+
+}
+
+const boundGreet = person.greeet.bind({ name: "Raj" })
+
+console.log(boundGreet)
--- interView/project.js
+++ interView/project.js
@@ -1,2 +1,4 @@
 const project1 = "use memo"
 const project2 = "what is closer int the javacriot "
+const project3 = "convert image into file 64 "
+    
