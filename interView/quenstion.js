c13335de4c2ec9d96012d6ad4e7eab1e2085859b
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-10 13:16:05 +0530

    que added what is bom element

--- interView/quenstion.js
+++ interView/quenstion.js
@@ -1,4 +1,4 @@
-const que1 = "what iss the diffrence between var variable and const variable" 
+const que1 = "what iss the diffrence between var variable and const variable"
 const ans1 = 'i) we can use variable before instioaling when use var but not for let '
 const que2 = "what is diffrecne between undifiend and not defiend" //// undefined a value or not defined a error whch make code stuck throw the error
 const que3 = 'What happend when you try to access the variable value before intilizing?'
@@ -7,3 +7,5 @@ const que5 = "how to use loading page into next js"
 const que6 = "library or framework me antar"
 const que7 = "how object work into the javasctipt "
 const que8 = "what is the main porpose to of new keyword in the object orianeted programming language"
+const que9 = "Bom full farm is browser object module"
+const que10 = ""
