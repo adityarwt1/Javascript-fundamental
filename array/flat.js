2cc19cd05c01d4a9d2b37c847841abb1e7d5f94d
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-26 16:04:42 +0530

    finnnaly added the some to collabrative feature

--- array/flat.js
+++ array/flat.js
@@ -1,11 +1,10 @@
-const unflated = [1,2,[3,4,5],6,[7,8,9]]
+const unflated = [1, 2, [3, 4, 5], 6, [7, 8, 9]];
 
-const flattedarray= unflated.flat()
+const flattedarray = unflated.flat();
 
-console.log(flattedarray)
+console.log(flattedarray);
 
-
-/// when we are use fla method to make a clean arraay elements 
-/// when we use flat() empty it removes emty slot from the array and 
+/// when we are use fla method to make a clean arraay elements
+/// when we use flat() empty it removes emty slot from the array and
 /// when we use flat(1) making array flat level 1 only
 /// when we use flat(infinity) make fully flat all time
