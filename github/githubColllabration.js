242362f6d8fcc82f4e09deaf42cec831fc24e4a9
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-26 18:06:52 +0530

    git add the commit

--- github/githubColllabration.js
+++ github/githubColllabration.js
@@ -3,4 +3,7 @@
 // git config --list list all those information who are privided by me and get set by the git
 // git config --global init.defaultBranch <name > this command make globlally by default nahi for the branch in initial point only
 // git branch -m main this change the name of the branch
-// 
+// git init // basically this commat initial the git control the local directory
+// git add . /// add all changes into the stanched stages
+// git commit -m "a meesage you added to the commit"
+//
