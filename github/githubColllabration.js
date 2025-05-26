2497381993cdbe10f916d899c2ad49a2af0426d1
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-26 17:49:21 +0530

    how to change the branch name

--- github/githubColllabration.js
+++ github/githubColllabration.js
@@ -2,3 +2,5 @@
 // git status whe a director controlled by the git 
 // git config --list list all those information who are privided by me and get set by the git
 // git config --global init.defaultBranch <name > this command make globlally by default nahi for the branch in initial point only
+// git branch -m main this change the name of the branch
+// 
