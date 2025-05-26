ca320a7425e1b87b06521d3a17fb6fffa1a03b61
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-26 17:45:19 +0530

    git intial branch name added into the github

--- github/githubColllabration.js
+++ github/githubColllabration.js
@@ -1,4 +1,4 @@
 // git verison show the git version 
 // git status whe a director controlled by the git 
 // git config --list list all those information who are privided by me and get set by the git
-//
+// git config --global init.defaultBranch <name > this command make globlally by default nahi for the branch in initial point only
