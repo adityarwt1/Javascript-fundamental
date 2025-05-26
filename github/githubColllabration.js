fee4fb1d3b16a26a83833e7aab45898415f20949
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-26 17:40:06 +0530

    git confg value added why this property needed into thhe developement

--- github/githubColllabration.js
+++ github/githubColllabration.js
@@ -1,3 +1,4 @@
 // git verison show the git version 
 // git status whe a director controlled by the git 
+// git config --list list all those information who are privided by me and get set by the git
 //
