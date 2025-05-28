2b49196c2de0b4f85389e5a6377ee5b0803084ac
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-28 11:08:07 +0530

    succeess message desplay and proper redirect to slash route

--- Dovelopement/filetobase64.js
+++ Dovelopement/filetobase64.js
@@ -1,36 +1,35 @@
 function fileToBase64(file) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
 
-        reader.onload = function () {
-            resolve(reader.result);
-        };
+    reader.onload = function () {
+      resolve(reader.result);
+    };
 
-        reader.onerror = function (error) {
-            reject(error);       
-        };
+    reader.onerror = function (error) {
+      reject(error);
+    };
 
-        reader.readAsDataURL(file);
-    }); 
+    reader.readAsDataURL(file);
+  });
 }
-
 
 const ownEffort = (file) => {
-    /// fucntion return promise so we use resolve and reject as well 
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader()
+  /// fucntion return promise so we use resolve and reject as well
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
 
-        //// thhis function return changes in the file  
-        reader.onload = function () {
-            resolve(reader.result)
-        }
+    //// thhis function return changes in the file
+    reader.onload = function () {
+      resolve(reader.result);
+    };
 
-        /// this function also return the error when porforming the functon error int promise
-        reader.onerror = function (error) {
-            reject(this.error)
-        }
+    /// this function also return the error when porforming the functon error int promise
+    reader.onerror = function (error) {
+      reject(this.error);
+    };
 
-        /// this step contai  the file fonmat converted into the base64 
-        reader.readAsDataURL(file)
-    })
-}
+    /// this step contai  the file fonmat converted into the base64
+    reader.readAsDataURL(file);
+  });
+};
