3bab0117237a16d459b860bd49b1d60ed3088b9e
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-09-09 22:54:29 +0530

    delete is done

--- Dovelopement/postgres/query.js
+++ Dovelopement/postgres/query.js
@@ -3,11 +3,17 @@
 // id INT AUTO_INCREMENT ;
 // name VARCHAR(100) NOT NULL;
 // city VARCHAR(100) NOT NULL;
-// ) 
+// )
 
-
-
-/// for insert the data 
+/// for insert the data
 
 //  INSERT INTO person(id,name , city )
 //  VALUES (101,'ADITYA RAWAT', 'KUSHMI');
+
+//FOR UPDATE THE VALUES
+
+// UPDATE person SET city = 'SIDHI' WHERE name = 'ADITYA RAWAT';
+
+//DELETE THE DATA
+
+//DELETE
