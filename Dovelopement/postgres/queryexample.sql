32fc35f0b7eb8c440c4c5bb172f6026b1c15633e
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-09-10 15:59:57 +0530

    aggreatator function

--- Dovelopement/postgres/queryexample.sql
+++ Dovelopement/postgres/queryexample.sql
@@ -7,9 +7,10 @@ dept VARCHAR(100) NOT NULL DEFAULT 'IT',
 salary INT NOT NULL,
 hire_date DATE DEFAULT CURRENT_DATE
 )
-
+/* delete the whole table*/
 DROP TABLE employies;
 
+/*all values present in the table*/
 SELECT * FROM employies;
 
 
@@ -37,4 +38,62 @@ DELETE FROM employies;
 SELECT * FROM employies WHERE emp_id = 1 ;
 SELECT * FROM employies WHERE dept= 'HR' ;
 
-SELECT FROM employies 
+SELECT * FROM employies ORDER BY emp_id;
+
+/* select between two values*/
+
+SELECT * FROM employies WHERE dept = 'HR' OR dept= 'OPERATOR';
+
+/*matching valuest*/
+
+SELECT * FROM employies WHERE dept = 'HR' AND salary > 50000;
+
+/*in operator use*/
+
+SELECT * FROM employies WHERE dept IN ('HR' , 'OPERATOR');
+
+/*NOT IN operator use*/
+
+SELECT * FROM employies WHERE dept NOT IN ('HR' , 'OPERATOR');
+
+
+/*BETWEEN OPERATOR*/
+
+SELECT * FROM employies WHERE salary BETWEEN 40000 AND 50000;
+
+
+/* usnique values in the collum*/
+
+SELECT DISTINCT dept FROM employies;
+
+
+/* shorting of the accendig order data*/
+
+SELECT * FROM employies ORDER BY fname;
+SELECT * FROM employies ORDER BY dept;
+
+
+/* shorting data according to the desceding order*/
+
+SELECT * FROM employies ORDER BY salary DESC;
+SELECT * FROM employies ORDER BY emp_id DESC;
+
+/*limitintg*/
+
+SELECT * FROM employies  LIMIT 3 ;
+
+/*like operator*/
+
+SELECT * FROM employies WHERE fname LIKE '%i%';
+
+/* where only found the only two characrter*/
+
+SELECT * FROM employies WHERE dept LIKE '__';
+
+/*COUNT THE DATA RECORD*/
+
+SELECT COUNT(emp_id) FROM employies;
+
+/*sum aggregator*/
+
+SELECT SUM(salary) FROM  employies;
