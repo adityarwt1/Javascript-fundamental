08035a4ba34575c45a53044905c519e6cfff3e2b
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-09-10 13:16:34 +0530

    sql quesry adde

---
+++ Dovelopement/postgres/queryexample.sql
@@ -0,0 +1,33 @@
+CREATE TABLE employies (
+emp_id SERIAL NOT NULL,
+fname VARCHAR(100) NOT NULL,
+lname VARCHAR(100) NOT NULL,
+email VARCHAR(100) NOT NULL UNIQUE,
+dept VARCHAR(100) NOT NULL DEFAULT 'IT',
+salary INT NOT NULL,
+hire_date DATE DEFAULT CURRENT_DATE
+)
+
+DROP TABLE employies;
+
+SELECT * FROM employies;
+
+
+// 
+
+UPDATE employies SET dept = 'DEVELOPER' WHERE emp_id = 1;
+
+
+INSERT INTO employies(fname , lname, email, dept, salary) VALUES ('Aditya' , 'Rawat' , 'adityarawat2487@gmail.com' , 'HR' , 60000 );
+INSERT INTO employies(fname , lname , email, dept , salary) VALUES ('Aman' , 'Rawat' , 'amanrawat26022004@gmail.com' , 'OPERATOR' , 30000  );
+INSERT INTO employies(fname , lname , email, dept , salary) VALUES ('Raj' , 'Sharma' , 'rajsharma@gmai.com' , 'IT' , 50000 );
+INSERT INTO employies(fname , lname , email, dept , salary) VALUES ('Priya' , 'Singh' , 'priyasingh@gmail.com' , 'HR' , 45000 );
+INSERT INTO employies(fname , lname , email, dept , salary) VALUES ('Arjun' , 'Verjma' , 'arjunverma@gmail.com' , 'DEVELOPER' , 50000 );
+INSERT INTO employies(fname , lname , email, dept , salary) VALUES ('Suman' , 'Patel' , 'sumanpate@gmail.com' , 'FINANCE' , 60000 );
+INSERT INTO employies(fname , lname , email, dept , salary) VALUES ('Vijay' , 'Nair' , 'vijyanair@gmail.com' , 'MARKETING' , 52000 );
+
+
+
+ALTER TABLE majdoor RENAME TO employies;
+
+DELETE FROM employies;
